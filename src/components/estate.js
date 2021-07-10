77c3940b7ddea317fb889666cc5951ddf6d37077
yash <yashlodhiya25@gmail.com>
2021-07-10 23:31:38 +0530

    second

--- src/components/estate.js
+++ src/components/estate.js
@@ -19,17 +19,16 @@ export default function estate(){
   }, [id])
 const ten = 10;
   return(
-    <div>
+    <div className="bluee">
       {
        posts.slice(0, 5).map(post => (
-        <div className="p-10" id={post.id}>
+        <div className="py-5 px-32 mx-auto " id={post.id}>
         <div className=" w-full lg:max-w-full lg:flex">
           <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url(${post.url})`}} title="Mountain">
           </div>
           <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
             <div className="mb-8">
-
-              <div className="text-gray-900 font-bold text-xl mb-2">Best Mountain Trails 2020</div>
+              <div className="text-gray-900 font-bold text-xl mb-2">Property no. : {post.id} </div>
               <p className="text-gray-700 text-base">{post.title}</p>
             </div>
             <div className="flex items-center">
